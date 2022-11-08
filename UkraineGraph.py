1ccba8db6c0a8ad86a390223cdf102ce8d7a0a33
LeonardoCBoar <43409820+LeonardoCBoar@users.noreply.github.com>
2022-11-08 12:41:34 -0300

    Fixed parsing

--- UkraineGraph.py
+++ UkraineGraph.py
@@ -2,32 +2,36 @@ import networkx
 import matplotlib
 import csv
 
-# Instancia objeto do Grafo
+# Instantiate Graph object
 ukraine = networkx.Graph()
 
-# Matrix de adjacencia
+# Read city data from csv file
 ukraine_matrix_file = open("Ukraine.csv", 'r')
 csv_reader = csv.reader(ukraine_matrix_file, delimiter=',')
 
+# Iterate trough the file, generating the graph matrix
 ukraine_matrix = []
 for line in ukraine_matrix_file:
-    ukraine_matrix.append(line.replace('\n', '').split(','))
+    city_connections = []
 
+    for number in line.replace('\n', '').replace(' ', '').split(','):
+        city_connections.append(int(number))
 
+    ukraine_matrix.append(city_connections)
+
+# Add nodes from the matrix
 for city_index in range(len(ukraine_matrix)):
     ukraine.add_node(city_index+1)
 
+# Add edges between nodes
 for city_index, city in enumerate(ukraine_matrix):
     for connection in city:
         ukraine.add_edge(city_index+1, connection)
 
+print(networkx.node_connectivity(ukraine))
+print(networkx.edge_connectivity(ukraine))
 
-# Conecta nós 2,3
-ukraine.add_edge(2, 3)
-
-# Renderiza nós na tela
+# Render graph on screen
 networkx.draw_networkx(ukraine, with_labels=True)
 matplotlib.pyplot.show()
 
-networkx.node_connectivity(ukraine)
-networkx.edge_connectivity(ukraine)
